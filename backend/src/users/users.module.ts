f8e29027557c61528b5d034768765d9364a0f410
Keltoum Malouki <keltoummalouki@gmail.com>
2026-02-05 09:44:02 +0100

    EBA-5 user registration with bcrypt #done

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
 
 @Module({
-  controllers: [UsersController],
+  imports: [
+    // Indispensable pour que @InjectRepository(User) fonctionne dans le service
+    TypeOrmModule.forFeature([User])
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService] 
 })
 export class UsersModule {}
